43477cec269db1431fd06d39606ea3d75b71fc48
Peter V <peter@vavro.me>
2022-07-11 15:12:24 -0500

    Add "About me" text

---
+++ components/SectionAboutMe.vue
@@ -0,0 +1,30 @@
+<!-- Please remove this file from your project -->
+<template>
+  <div class="bg-primary-contrast p-20">
+    <h1 class="text-3xl pb-3">
+      Peter Vavro<br /><small>Full-Stack Engineer / Scrum Master</small>
+    </h1>
+    <section>
+      <h2 class="text-2xl pb-2">About me</h2>
+      <p>
+        I am a Full-Stack Engineer / Scrum Master, specialized in JavaScript,
+        interested in programming since 1994.
+      </p>
+      <p>
+        My experience includes 13+ years with JS and I am efficient in both,
+        front and back end. In front-end web and mobile: React (<span
+          class="react-years"
+          >5</span
+        >+ years), React Native (<span class="react-native-years">4</span>+
+        years), Vue.js, AngularJS and many other interesting frameworks and
+        libraries. In back-end, my experience includes
+        <span class="nodejs-years">7 </span>+ years with Node.js working with
+        various frameworks such as Express.js, Sails.js, LoopBack and HapiJS.
+      </p>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,4 @@
 <template>
   <MainIntro />
+  <SectionAboutMe />
 </template>
