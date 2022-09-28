7a866b491580b87ee0ee8cac508ea680d736538d
Peter V <peter@vavro.me>
2022-09-28 15:35:21 -0500

    Adjust content of section "intro" for mobile users

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,12 @@
 <template>
-	<MainIntro />
-	<SectionAboutMe id="about-me" />
-	<SectionTechnologies id="technologies" />
-	<SectionCodeSamples id="code-samples" />
-	<SectionProjects id="projects" />
+    <SectionIntro />
+    <SectionAboutMe id="about-me" />
+    <SectionTechnologies id="technologies" />
+    <SectionCodeSamples id="code-samples" />
+    <SectionProjects id="projects" />
 </template>
 <style>
 :target {
-	scroll-margin-top: 100px;
+    scroll-margin-top: 100px;
 }
 </style>
