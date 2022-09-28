596d9b4a04bfb1a9afa37c52922af9cf3e968b02
Peter V <peter@vavro.me>
2022-09-28 15:21:11 -0500

    Make main menu scroll to targets

--- components/MainMenu.vue
+++ components/MainMenu.vue
@@ -1,59 +1,63 @@
 <template>
-	<div>
-		<button
-			id="show-modal"
-			@click="showModal = true"
-			class="p-3 text-tertiary border-tertiary hover:text-secondary hover:border-secondary border-2"
-		>
-			<IconMenu class="w-6 h-6" />
-		</button>
-		<Teleport to="body">
-			<modal :show="showModal" @close="showModal = false">
-				<div class="flex flex-col items-center p-10">
-					<div class="pb-5">
-						<ul class="flex flex-col">
-							<li class="px-2 py-2">
-								<a href="/intro" class="text-xl transition-all duration-300">
-									Intro
-								</a>
-							</li>
-							<li class="px-2 py-2">
-								<a href="/me" class="text-xl transition-all duration-300">
-									About me
-								</a>
-							</li>
-							<li class="px-2 py-2">
-								<a
-									href="/code-samples"
-									class="text-xl transition-all duration-300"
-								>
-									Code
-								</a>
-							</li>
-							<li class="px-2 py-2">
-								<a href="/projects" class="text-xl transition-all duration-300">
-									Projects
-								</a>
-							</li>
-						</ul>
-					</div>
-					<div class="pb-5">
-						<PanelContact inverted="true" />
-					</div>
-				</div>
-			</modal>
-		</Teleport>
-	</div>
+    <div>
+        <button
+            id="show-modal"
+            @click="showModal = true"
+            class="p-3 text-tertiary border-tertiary hover:text-secondary hover:border-secondary border-2"
+        >
+            <IconMenu class="w-6 h-6" />
+        </button>
+        <Teleport to="body">
+            <modal :show="showModal" @close="showModal = false">
+                <div class="flex flex-col items-center p-10">
+                    <div class="pb-5">
+                        <ul class="flex flex-col">
+                            <li class="px-2 py-2">
+                                <a
+                                    href="#about-me"
+                                    @click="showModal = false"
+                                    class="text-xl transition-all duration-300"
+                                >
+                                    About me
+                                </a>
+                            </li>
+                            <li class="px-2 py-2">
+                                <a
+                                    href="#code-samples"
+                                    @click="showModal = false"
+                                    class="text-xl transition-all duration-300"
+                                >
+                                    Code
+                                </a>
+                            </li>
+                            <li class="px-2 py-2">
+                                <a
+                                    href="#projects"
+                                    @click="showModal = false"
+                                    class="text-xl transition-all duration-300"
+                                >
+                                    Projects
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="pb-5">
+                        <PanelContact inverted="true" />
+                    </div>
+                </div>
+            </modal>
+        </Teleport>
+    </div>
 </template>
 <script lang="ts">
 import IconMenu from '~/assets/icon-menu.svg'
 
 export default {
-	components: { IconMenu },
-	data() {
-		return {
-			showModal: false
-		}
-	}
+    components: { IconMenu },
+    data() {
+        return {
+            showModal: false
+        }
+    }
 }
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,12 @@
 <template>
-    <MainIntro />
-    <SectionAboutMe />
-    <SectionTechnologies />
-    <SectionCodeSamples />
-    <SectionProjects />
+	<MainIntro />
+	<SectionAboutMe id="about-me" />
+	<SectionTechnologies id="technologies" />
+	<SectionCodeSamples id="code-samples" />
+	<SectionProjects id="projects" />
 </template>
+<style>
+:target {
+	scroll-margin-top: 100px;
+}
+</style>
