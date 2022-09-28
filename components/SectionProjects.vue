1b37b37d7edd5669926b6e832ced29b52478f16d
Peter V <peter@vavro.me>
2022-09-28 15:20:33 -0500

    Add transitions to "projects" section

--- components/SectionProjects.vue
+++ components/SectionProjects.vue
@@ -1,16 +1,25 @@
+<script>
+export default {
+	setup() {
+		const { target, isVisible } = useIsInViewport()
+
+		return {
+			target,
+			isVisible
+		}
+	}
+}
+</script>
+
 <template>
-	<div class="py-28 container mx-auto lg:max-w-4xl">
-		<section class="p-5 sm:p-10 mt-10 text-secondary">
-			<h2 class="text-2xl drop-shadow-md pb-10 uppercase">
-				Things I've helped to build
-			</h2>
-			<list-projects />
-		</section>
+	<div ref="target">
+		<div class="py-28 container mx-auto lg:max-w-4xl">
+			<section class="p-5 sm:p-10 text-secondary">
+				<h2 class="text-2xl drop-shadow-md pb-10 uppercase">
+					Things I've helped to build
+				</h2>
+				<list-projects v-if="isVisible" />
+			</section>
+		</div>
 	</div>
 </template>
-<script>
-export default {}
-</script>
-<style lang="">
-    
-</style>
