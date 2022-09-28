8d6ff5ae7d7e288a4314d83c3a68d0566feee523
Peter V <peter@vavro.me>
2022-09-28 15:47:31 -0500

    Disable top close button in modal for smaller screens

--- components/Modal.vue
+++ components/Modal.vue
@@ -1,60 +1,60 @@
 <template>
-    <Transition name="modal">
-        <div
-            v-if="show"
-            class="fixed top-0 left-0 w-full h-full z-20 display-block bg-secondary overflow-x-hidden overflow-y-auto"
-        >
-            <div class="w-screen max-w-none h-full m-0 relative">
-                <div class="relative flex flex-col w-full h-full">
-                    <div
-                        class="overflow-y-auto relative grow shrink basis-auto"
-                    >
-                        <div
-                            class="flex shrink-0 items-center justify-between p-10 my-3"
-                        >
-                            <div></div>
-                            <div>
-                                <button
-                                    @click="$emit('close')"
-                                    class="border-neutral border-2 p-3 text-neutral"
-                                    ref="target"
-                                >
-                                    Close
-                                </button>
-                            </div>
-                        </div>
-                        <slot />
-                        <div class="w-full text-center pb-10">
-                            <button
-                                @click="$emit('close')"
-                                class="border-neutral border-2 p-3 text-neutral"
-                            >
-                                Close
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </Transition>
+	<Transition name="modal">
+		<div
+			v-if="show"
+			class="fixed top-0 left-0 w-full h-full z-20 display-block bg-secondary overflow-x-hidden overflow-y-auto"
+		>
+			<div class="w-screen max-w-none h-full m-0 relative">
+				<div class="relative flex flex-col w-full h-full">
+					<div
+						class="overflow-y-auto relative grow shrink basis-auto"
+					>
+						<div
+							class="p-10 my-3 hidden md:flex shrink-0 items-center justify-between"
+						>
+							<div></div>
+							<div>
+								<button
+									@click="$emit('close')"
+									class="border-neutral border-2 p-3 text-neutral"
+									ref="target"
+								>
+									Close
+								</button>
+							</div>
+						</div>
+						<slot />
+						<div class="w-full text-center pb-10">
+							<button
+								@click="$emit('close')"
+								class="border-neutral border-2 p-3 text-neutral"
+							>
+								Close
+							</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</Transition>
 </template>
 
 <script>
 export default {
-    props: {
-        show: Boolean
-    }
+	props: {
+		show: Boolean
+	}
 }
 </script>
 
 <style scoped>
 .modal-enter-active,
 .modal-leave-active {
-    @apply transition-all duration-300 ease-out;
+	@apply transition-all duration-300 ease-out;
 }
 
 .modal-enter-from,
 .modal-leave-to {
-    @apply opacity-0;
+	@apply opacity-0;
 }
 </style>
