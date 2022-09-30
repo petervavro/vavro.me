9cce1a89ea0be73a0e9a909a42c1a6b3e8784bec
Peter V <peter@vavro.me>
2022-09-29 22:59:07 -0500

    Adjust paddings

--- components/TheThumbnail.vue
+++ components/TheThumbnail.vue
@@ -1,21 +1,25 @@
 <template>
-	<div>
-		<div class="text-xs pb-3 text-center">{{ title }}</div>
-		<div
-			v-if="thumbnailImage"
-			class="w-20 h-20 rounded-full bg-secondary/50 border-2 border-secondary bg-[length:50%] bg-no-repeat bg-center contrast-125"
-			:style="{
-				'background-image': `url(${thumbnailImage})`
-			}"
-		></div>
-	</div>
+    <div class="group opacity-90">
+        <div
+            class="text-xs pb-3 text-center group-hover:text-primary transition duration-300"
+        >
+            {{ title }}
+        </div>
+        <div
+            v-if="thumbnailImage"
+            class="w-20 h-20 rounded-full bg-secondary/50 group-hover:bg-primary/50 border-2 border-secondary group-hover:border-primary bg-[length:50%] bg-no-repeat bg-center contrast-125 transition duration-300"
+            :style="{
+                'background-image': `url(${thumbnailImage})`
+            }"
+        ></div>
+    </div>
 </template>
 <script>
 export default {
-	props: {
-		url: String,
-		title: String,
-		thumbnailImage: String
-	}
+    props: {
+        url: String,
+        title: String,
+        thumbnailImage: String
+    }
 }
 </script>
