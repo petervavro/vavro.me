53e4bf9357bec9c4a9e1b97e912270c97a9dc470
Peter V <peter@vavro.me>
2022-09-29 21:46:51 -0500

    Improve hover responses in main menu modal

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -3,22 +3,22 @@ export default {}
 </script>
 	
 <template>
-    <div class="py-10 md:p-20 text-primary text-center md:text-left">
-        <div
-            class="rounded-full border-2 border-secondary p-2 inline-flex mb-10 items-center"
-        >
-            <div class="p-3">Contact me</div>
-            <PanelContact :is-github-visible="false" />
-        </div>
-        <div class="md:flex md:flex-row-reverse justify-between items-center">
-            <a href="#" class="border-2 border-secondary p-3 display-block">
-                Back to top
-            </a>
-            <div class="p-10 py-16 md:p-0">
-                All rights reserved © Peter Vavro 2013-{{
-                    new Date().getFullYear()
-                }}
-            </div>
-        </div>
-    </div>
+	<div class="py-10 md:p-20 text-primary text-center md:text-left">
+		<div
+			class="p-1 rounded-full border-2 border-secondary inline-flex mb-10 items-center"
+		>
+			<div class="p-3">Contact me</div>
+			<PanelContact :is-github-visible="false" />
+		</div>
+		<div class="md:flex md:flex-row-reverse justify-between items-center">
+			<a href="#" class="p-2 border-2 border-secondary display-block">
+				Back to top
+			</a>
+			<div class="p-10 py-16 md:p-0">
+				All rights reserved © Peter Vavro 2013-{{
+					new Date().getFullYear()
+				}}
+			</div>
+		</div>
+	</div>
 </template>
