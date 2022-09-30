4906683acd42e07eafe9366b8c14e36bee4d94a8
Peter V <peter@vavro.me>
2022-09-29 21:30:35 -0500

    Change space height between sections

--- components/SectionCodeSamples.vue
+++ components/SectionCodeSamples.vue
@@ -1,26 +1,26 @@
 
 <script>
 export default {
-    setup() {
-        const { target, isVisible } = useIsInViewport()
+	setup() {
+		const { target, isVisible } = useIsInViewport()
 
-        return {
-            target,
-            isVisible
-        }
-    }
+		return {
+			target,
+			isVisible
+		}
+	}
 }
 </script>
 
 <template>
-    <div ref="target">
-        <div class="py-28 container mx-auto lg:max-w-4xl">
-            <section class="p-10 text-tertiary">
-                <h2 class="text-2xl pb-5 uppercase text-primary">
-                    My code samples
-                </h2>
-                <ListCodeSamples v-if="isVisible" />
-            </section>
-        </div>
-    </div>
+	<div ref="target">
+		<div class="container mx-auto lg:max-w-4xl">
+			<section class="p-10 text-tertiary">
+				<h2 class="text-2xl pb-5 uppercase text-primary">
+					My code samples
+				</h2>
+				<ListCodeSamples v-if="isVisible" />
+			</section>
+		</div>
+	</div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,9 @@
 <template>
 	<SectionIntro />
-	<SectionAboutMe id="about-me" />
-	<SectionTechnologies id="technologies" />
-	<SectionCodeSamples id="code-samples" />
-	<SectionProjects id="projects" />
+	<SectionAboutMe id="about-me" class="py-20" />
+	<SectionTechnologies id="technologies" class="py-20" />
+	<SectionCodeSamples id="code-samples" class="py-20" />
+	<SectionProjects id="projects" class="py-20" />
 </template>
 <style>
 :target {
