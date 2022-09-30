868ef9de560d4e0c06897c889cf42da613beb4c5
Peter V <peter@vavro.me>
2022-09-29 20:52:10 -0500

    Remove header title animation

--- components/SectionCodeSamples.vue
+++ components/SectionCodeSamples.vue
@@ -1,43 +1,26 @@
 
 <script>
 export default {
-	setup() {
-		const { target, isVisible } = useIsInViewport()
+    setup() {
+        const { target, isVisible } = useIsInViewport()
 
-		return {
-			target,
-			isVisible
-		}
-	}
+        return {
+            target,
+            isVisible
+        }
+    }
 }
 </script>
 
 <template>
-	<div ref="target">
-		<div class="py-28 container mx-auto lg:max-w-4xl">
-			<section class="p-10 text-tertiary">
-				<Transition name="title">
-					<h2
-						class="text-2xl pb-5 uppercase text-primary"
-						v-if="isVisible"
-					>
-						My code samples
-					</h2>
-				</Transition>
-				<ListCodeSamples v-if="isVisible" />
-			</section>
-		</div>
-	</div>
+    <div ref="target">
+        <div class="py-28 container mx-auto lg:max-w-4xl">
+            <section class="p-10 text-tertiary">
+                <h2 class="text-2xl pb-5 uppercase text-primary">
+                    My code samples
+                </h2>
+                <ListCodeSamples v-if="isVisible" />
+            </section>
+        </div>
+    </div>
 </template>
-
-<style scoped>
-.title-enter-active,
-.title-leave-active {
-	@apply transition duration-500 ease-in-out;
-}
-
-.title-enter-from,
-.title-leave-to {
-	@apply opacity-0;
-}
-</style>
