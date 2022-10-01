cd5029cafdd198f182436c7dc2699e3ead1d71c1
Peter V <peter@vavro.me>
2022-10-01 10:09:17 -0500

    Simplify page with templates

---
+++ components/TheSection.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+    setup() {
+        const { target, isVisible } = useIsInViewport()
+
+        return {
+            target,
+            isVisible
+        }
+    },
+    props: {
+        title: String
+    }
+}
+</script>
+        
+<template>
+    <div ref="target">
+        <div class="container mx-auto lg:max-w-4xl">
+            <section class="p-10">
+                <h2 class="text-2xl pb-5 uppercase text-primary">
+                    {{ title }}
+                </h2>
+                <slot v-if="isVisible" />
+            </section>
+        </div>
+    </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,33 @@
 <template>
-    <Head>
-        <Title> Peter Vavro = Full-Stack Engineer/Scrum Master </Title>
-    </Head>
-    <SectionIntro />
-    <SectionAboutMe id="about-me" class="py-20" />
-    <SectionTechnologies id="technologies" class="py-20" />
-    <SectionCodeSamples id="code-samples" class="py-20" />
-    <SectionProjects id="projects" class="py-20" />
+	<Head>
+		<Title> Peter Vavro = Full-Stack Engineer/Scrum Master </Title>
+	</Head>
+	<SectionIntro />
+	<SectionAboutMe id="about-me" class="py-20" />
+	<TheSection
+		id="technologies"
+		title="My experience"
+		class="py-20 text-primary"
+	>
+		<ListTechnologies />
+	</TheSection>
+	<TheSection
+		id="code-samples"
+		title="My code samples"
+		class="py-20 text-tertiary"
+	>
+		<ListCodeSamples />
+	</TheSection>
+	<TheSection
+		id="projects"
+		title="Things I've helped to build"
+		class="py-20 text-secondary"
+	>
+		<ListProjects />
+	</TheSection>
 </template>
 <style>
 :target {
-    scroll-margin-top: 100px;
+	scroll-margin-top: 100px;
 }
 </style>
