633c433e8c3e53273cc33a16ae91631d0769d919
Peter V <peter@vavro.me>
2022-10-02 10:18:59 -0500

    Make hover of contact buttons more visible

--- components/PanelContact.vue
+++ components/PanelContact.vue
@@ -1,56 +1,44 @@
 <template>
-    <ul class="flex flex-row items-center">
-        <li class="p-2">
-            <a
-                href="mailto:<%= mailto %> "
-                title="My Email"
-                rel="noreferrer"
-                class="block p-3 border-2 rounded-full transform hover:scale-110 transition duration-300"
-                :class="[
-                    inverted
-                        ? 'text-neutral border-neutral hover:text-blue-50:border-blue-50'
-                        : 'text-primary border-primary hover:text-blue-50:border-blue-50'
-                ]"
-            >
-                <IconEmail />
-                <span class="sr-only"> My Email </span>
-            </a>
-        </li>
-        <li class="p-2">
-            <a
-                href="https://www.linkedin.com/in/peter-vavro-44604163"
-                target="_blank"
-                title="My Linkedin"
-                rel="noreferrer"
-                class="block p-2 border-2 rounded-full transform hover:scale-110 transition duration-300"
-                :class="[
-                    inverted
-                        ? 'text-neutral border-neutral hover:text-blue-50:border-blue-50'
-                        : 'text-primary border-primary hover:text-blue-50:border-blue-50'
-                ]"
-            >
-                <IconLinkedIn />
-                <span class="sr-only"> My Linkedin </span>
-            </a>
-        </li>
-        <li v-if="isGithubVisible" class="p-2">
-            <a
-                href="https://github.com/petervavro"
-                target="_blank"
-                title="My Github"
-                rel="noreferrer"
-                class="block p-3 border-2 rounded-full transform hover:scale-110 transition duration-300"
-                :class="[
-                    inverted
-                        ? 'text-neutral border-neutral hover:text-blue-50:border-blue-50'
-                        : 'text-primary border-primary hover:text-blue-50:border-blue-50'
-                ]"
-            >
-                <IconGithub />
-                <span class="sr-only"> My GitHub </span>
-            </a>
-        </li>
-    </ul>
+	<ul class="flex flex-row items-center">
+		<li class="p-2">
+			<a
+				href="mailto:<%= mailto %> "
+				title="My Email"
+				rel="noreferrer"
+				class="p-3"
+				:class="{ inverted: inverted }"
+			>
+				<IconEmail />
+				<span class="sr-only"> My Email </span>
+			</a>
+		</li>
+		<li class="p-2">
+			<a
+				href="https://www.linkedin.com/in/peter-vavro-44604163"
+				target="_blank"
+				title="My Linkedin"
+				rel="noreferrer"
+				class="p-2"
+				:class="{ inverted: inverted }"
+			>
+				<IconLinkedIn />
+				<span class="sr-only"> My Linkedin </span>
+			</a>
+		</li>
+		<li v-if="isGithubVisible" class="p-2">
+			<a
+				href="https://github.com/petervavro"
+				target="_blank"
+				title="My Github"
+				rel="noreferrer"
+				class="p-3"
+				:class="{ inverted: inverted }"
+			>
+				<IconGithub />
+				<span class="sr-only"> My GitHub </span>
+			</a>
+		</li>
+	</ul>
 </template>
 <script>
 import IconEmail from '~/assets/icon-Email.svg'
@@ -58,16 +46,26 @@ import IconGithub from '~/assets/logo-Github.svg'
 import IconLinkedIn from '~/assets/icon-LinkedIn.svg'
 
 export default {
-    components: { IconEmail, IconGithub, IconLinkedIn },
-    props: {
-        inverted: {
-            type: Boolean,
-            default: false
-        },
-        isGithubVisible: {
-            type: Boolean,
-            default: true
-        }
-    }
+	components: { IconEmail, IconGithub, IconLinkedIn },
+	props: {
+		inverted: {
+			type: Boolean,
+			default: false
+		},
+		isGithubVisible: {
+			type: Boolean,
+			default: true
+		}
+	}
 }
 </script>
+
+<style scoped>
+ul a {
+	@apply block border-2 text-primary border-primary hover:text-secondary hover:border-secondary rounded-full transform hover:scale-110 transition duration-300;
+}
+
+ul a.inverted {
+	@apply block border-2 rounded-full transform hover:scale-110 transition duration-300 text-neutral border-neutral hover:text-neutral hover:border-neutral;
+}
+</style>
