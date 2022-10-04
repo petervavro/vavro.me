bddc225f5320561bc5465d46952026997b330214
Peter V <peter@vavro.me>
2022-10-04 16:23:57 -0500

    Add hint to console

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,3 +1,13 @@
+<script>
+export default {
+	mounted() {
+		console.log(
+			"Hey 👋! You're probably wondering what could be the code behind my web page, so feel free to check it out at https://github.com/petervavro/vavro.me . Also, you can contact me at peter@vavro.me"
+		)
+	}
+}
+</script>
+	
 <template>
 	<main class="tracking-tight">
 		<TheHeader />
