1685b36caafaa4e3ba77ffda39e99a0212cd7b79
Peter V <peter@vavro.me>
2022-12-28 19:24:19 -0500

    Improve animation in "Codes" section

--- components/ListCodeSamples.vue
+++ components/ListCodeSamples.vue
@@ -1,9 +1,6 @@
 <script setup>
 import TECHNOLOGIES from 'assets/data/technologies.json'
 import JSON_DATA from 'assets/data/codes.json'
-import gsap from 'gsap'
-
-const isVisible = ref(true)
 
 const items = JSON_DATA.map(({ technologies, ...rest }) => ({
   ...rest,
@@ -16,41 +13,18 @@ const items = JSON_DATA.map(({ technologies, ...rest }) => ({
     })
   }))
 }))
-
-const onBeforeEnter = (el) => {
-  el.style.opacity = 0
-}
-const onEnter = (el, done) => {
-  gsap.to(el, {
-    opacity: 1,
-    delay: el.dataset.index * 0.3,
-    onComplete: done
-  })
-}
-const onLeave = (el, done) => {
-  gsap.to(el, {
-    opacity: 0,
-    onComplete: done
-  })
-}
 </script>
 
 <template>
-  <TransitionGroup
-    tag="div"
-    class="grid grid-cols md:grid-cols-2 lg:grid-cols-3 gap-3 group"
-    :css="false"
-    @before-enter="onBeforeEnter"
-    @enter="onEnter"
-    @leave="onLeave"
-  >
+  <div class="grid grid-cols md:grid-cols-2 lg:grid-cols-3 gap-3 group">
     <a
       :href="item.url"
       target="_blank"
       v-for="(item, index) in items"
-      class="shadow-lg shadow-black/10 p-5 border-neutral-light/50 border-t border-l hover:border-secondary hover:text-secondary transition-all duration-300 ease-out"
+      class="shadow-lg shadow-black/10 p-5 border-neutral-light/50 border-t border-l hover:border-dotted hover:border-secondary hover:text-secondary transition-all duration-500 ease-out"
       :key="item.title"
       :data-index="index"
+      v-display-when-in-view
     >
       <h3 class="h-16 text-xl pb-3">
         {{ item.title }}
@@ -63,5 +37,5 @@ const onLeave = (el, done) => {
         class="pr-2 pb-2 inline-block scale-90 border-secondary"
       />
     </a>
-  </TransitionGroup>
+  </div>
 </template>
