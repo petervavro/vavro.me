9bbb5b541e8d844153b841870c83bbb88d0f525a
Peter V <peter@vavro.me>
2022-12-29 17:06:34 -0500

    Organize better main page structure

--- pages/index.vue
+++ pages/index.vue
@@ -1,36 +1,36 @@
 <template>
-    <Head>
-        <Title> Peter Vavro = Full-Stack Engineer/Scrum Master </Title>
-    </Head>
-    <SectionIntro />
-    <SectionAboutMe id="about-me" class="py-20" />
-    <TheSection
-        id="technologies"
-        title="My experience"
-        class="py-20 text-primary"
-        titleClass="lg:text-4xl lg:-rotate-90 lg:-translate-x-[28rem] lg:-translate-y-24"
-    >
-        <ListTechnologies />
-    </TheSection>
-    <TheSection
-        id="code-samples"
-        title="My code samples"
-        class="py-20 text-tertiary"
-        titleClass="lg:text-4xl lg:-rotate-90 lg:-translate-x-[28rem] lg:-translate-y-16"
-    >
-        <ListCodeSamples />
-    </TheSection>
-    <TheSection
-        id="projects"
-        title="Things I've helped to build"
-        class="py-20 text-secondary"
-        titleClass="lg:text-4xl lg:-rotate-90 lg:-translate-x-[28rem] lg:translate-y-52"
-    >
-        <ListProjects />
-    </TheSection>
+  <Head>
+    <Title> Peter Vavro = Full-Stack Engineer/Scrum Master </Title>
+  </Head>
+  <SectionIntro />
+  <SectionAboutMe id="about-me" class="py-20" />
+  <TheSection
+    id="technologies"
+    title="My experience"
+    class="py-20 text-primary"
+    titleClass="lg:text-4xl lg:-rotate-90 lg:-translate-x-[28rem] lg:-translate-y-24"
+  >
+    <ListTechnologies />
+  </TheSection>
+  <TheSection
+    id="code-samples"
+    title="My code samples"
+    class="py-20 text-tertiary"
+    titleClass="lg:text-4xl lg:-rotate-90 lg:-translate-x-[28rem] lg:-translate-y-16"
+  >
+    <ListCodeSamples />
+  </TheSection>
+  <TheSection
+    id="projects"
+    title="Things I've helped to build"
+    class="py-20 text-secondary"
+    titleClass="lg:text-4xl lg:-rotate-90 lg:-translate-x-[28rem] lg:translate-y-52"
+  >
+    <ListProjects />
+  </TheSection>
 </template>
 <style>
 :target {
-    scroll-margin-top: 100px;
+  scroll-margin-top: 100px;
 }
 </style>
