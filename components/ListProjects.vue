119b5d2f6c14b2e2444456621526061b0363bce4
Peter V <peter@vavro.me>
2022-12-29 11:07:55 -0500

    Improve animation in "Projects" section

--- components/ListProjects.vue
+++ components/ListProjects.vue
@@ -1,90 +1,59 @@
-<script>
+<script setup>
 import TECHNOLOGIES from 'assets/data/technologies.json'
 import JSON_DATA from 'assets/data/projects.json'
-import gsap from 'gsap'
 
-export default {
-  data() {
-    return {
-      selectedProjectId: false,
-      items: JSON_DATA.map((item) => ({
-        ...item,
-        ...(item?.backgroundImage && {
-          backgroundImage: new URL(
-            `../assets/${item.backgroundImage}`,
-            import.meta.url
-          ).href
-        })
-      }))
-    }
-  },
-  methods: {
-    onBeforeEnter(el) {
-      el.style.opacity = 0
-    },
-    onEnter(el, done) {
-      gsap.to(el, {
-        opacity: 1,
-        delay: el.dataset.index * 0.15,
-        onComplete: done
+const selectedProjectId = ref(false)
+const items = ref(
+  JSON_DATA.map((item) => ({
+    ...item,
+    ...(item?.backgroundImage && {
+      backgroundImage: `/assets/${item.backgroundImage}`
+    })
+  }))
+)
+
+const projectData = computed(() => {
+  const project = items.value.find(({ id }) => id === selectedProjectId.value)
+  return {
+    ...project,
+    technologies: TECHNOLOGIES.filter(({ id }) =>
+      project.technologies.split(',').includes(id)
+    ).map((t) => ({
+      ...t,
+      ...(t?.thumbnailImage && {
+        thumbnailImage: new URL(
+          `../assets/${t.thumbnailImage}`,
+          import.meta.url
+        ).href
       })
-    },
-    onLeave(el, done) {
-      gsap.to(el, {
-        opacity: 0,
-        onComplete: done
-      })
-    }
-  },
-  watch: {
-    selectedProjectId(newValue) {
-      if (newValue !== false) {
-        document.body.classList.add('overflow-hidden')
-      } else {
-        document.body.classList.remove('overflow-hidden')
-      }
-    }
-  },
-  computed: {
-    projectData() {
-      const project = this.items.find(({ id }) => id === this.selectedProjectId)
-      return {
-        ...project,
-        technologies: TECHNOLOGIES.filter(({ id }) =>
-          project.technologies.split(',').includes(id)
-        ).map((t) => ({
-          ...t,
-          ...(t?.thumbnailImage && {
-            thumbnailImage: new URL(
-              `../assets/${t.thumbnailImage}`,
-              import.meta.url
-            ).href
-          })
-        }))
-      }
-    }
+    }))
   }
-}
+})
 </script>
 
 <template>
-  <modal :show="selectedProjectId" @close="selectedProjectId = false">
-    <article class="container mx-auto max-w-4xl p-10 pt-5">
-      <div class="bg-tertiary -xl p-10">
+  <modal
+    :show="selectedProjectId"
+    @close="selectedProjectId = false"
+    modalContentClass="bg-neutral text-secondary"
+    buttonClass="text-white/50 hover:text-white hover:bg-neutral-light border-white/50"
+  >
+    <article class="box mx-auto max-w-4xl md:p-10 pt-5">
+      <div class="p-10">
         <h3 class="text-2xl pb-3">{{ projectData.title }}</h3>
         <section class="pb-5">
-          <h4>PROJECT DESCRIPTION</h4>
+          <h4 class="pb-1">PROJECT DESCRIPTION</h4>
           <p class="pb-1">{{ projectData.description }}</p>
           <a
             :href="projectData.url"
             target="_blank"
             class="text-ellipsis overflow-hidden underline"
           >
-            View project.
+            Review the project.
           </a>
         </section>
         <section class="pb-5">
-          <h4>JOB DESCRIPTION</h4>
+          <h4 class="pb-1">JOB DESCRIPTION</h4>
           <p>{{ projectData.jobDescription }}</p>
           <p>{{ projectData.year }}</p>
         </section>
@@ -100,13 +69,9 @@ export default {
     </article>
   </modal>
   <div>
-    <TransitionGroup
-      tag="div"
-      class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3"
-      :css="false"
-      @before-enter="onBeforeEnter"
-      @enter="onEnter"
-      @leave="onLeave"
+    <div
+      class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 opacity-0 transition-all duration-700 ease-out"
+      v-display-when-in-view="0.1"
     >
       <button
         v-for="(item, index) in items"
@@ -115,9 +80,8 @@ export default {
         :data-index="index"
         @click="selectedProjectId = item.id"
         :class="[
-          'flex flex-row group text-primary hover:text-secondary transition-all duration-300 ease-out py-5 md:p-5'
+          'flex flex-row group text-primary hover:text-secondary py-5 md:p-5'
         ]"
-        v-display-when-in-view
       >
         <div
           :class="`flex-none w-20 h-20 rounded-50p group-hover:rounded-none shadow-black/80 shadow-inner group-hover:shadow-2xl group-hover:rotate-[360deg] border-primary border-2 group-hover:border-secondary transition-all duration-700 ease-out bg-white bg-cover bg-no-repeat bg-center saturate-150 ${
@@ -146,6 +110,33 @@ export default {
           </div>
         </div>
       </button>
-    </TransitionGroup>
+    </div>
   </div>
 </template>
+
+<style scoped>
+.box {
+  --border-angle: 0turn;
+  background-image: conic-gradient(
+    from var(--border-angle) at -2% -2%,
+    transparent 20%,
+    theme('colors.neutral.light'),
+    theme('colors.neutral.DEFAULT')
+  );
+  background-repeat: no-repeat;
+  background-position: center center;
+  animation: bg-spin 6s linear infinite;
+}
+
+@keyframes bg-spin {
+  to {
+    --border-angle: 1turn;
+  }
+}
+
+@property --border-angle {
+  syntax: '<angle>';
+  inherits: true;
+  initial-value: 0turn;
+}
+</style>
--- components/Modal.vue
+++ components/Modal.vue
@@ -1,74 +1,87 @@
-<template>
-    <Teleport to="body">
-        <Transition name="modal">
-            <div
-                v-if="show"
-                class="fixed top-0 left-0 w-full h-full z-20 display-block bg-secondary overflow-x-hidden overflow-y-auto"
-            >
-                <div class="w-screen max-w-none h-full m-0 relative">
-                    <div class="relative flex flex-col w-full h-full">
-                        <div
-                            class="overflow-y-auto relative grow shrink basis-auto"
-                        >
-                            <div
-                                class="p-11 my-3 hidden md:flex shrink-0 items-center justify-between"
-                            >
-                                <div></div>
-                                <div>
-                                    <button
-                                        @click="$emit('close')"
-                                        class="p-2 border-2 bg-secondary hover:bg-neutral border-neutral text-neutral hover:text-secondary transition duration-300"
-                                        ref="target"
-                                    >
-                                        Close
-                                    </button>
-                                </div>
-                            </div>
-                            <slot />
-                            <div class="w-full text-center pb-10">
-                                <button
-                                    @click="$emit('close')"
-                                    class="p-2 border-2 bg-secondary hover:bg-neutral border-neutral text-neutral hover:text-secondary transition duration-300"
-                                >
-                                    Close
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </Transition>
-    </Teleport>
-</template>
-
 <script>
 export default {
-    props: {
-        show: Boolean
-    },
-    methods: {
-        handleClose(e) {
-            if (e.keyCode === 27) this.$emit('close')
-        }
-    },
-    watch: {
-        show(newValue) {
-            if (newValue)
-                document.body.addEventListener('keyup', this.handleClose)
-            else document.body.removeEventListener('keyup', this.handleClose)
-        }
+  props: {
+    show: Boolean,
+    modalContentClass: String,
+    buttonClass: String
+  },
+  methods: {
+    handleClose(e) {
+      if (e.keyCode === 27) this.$emit('close')
     }
+  },
+  watch: {
+    show(newValue) {
+      if (newValue) {
+        document.body.addEventListener('keyup', this.handleClose)
+
+        document.body.classList.add('overflow-hidden')
+      } else {
+        document.body.removeEventListener('keyup', this.handleClose)
+
+        document.body.classList.remove('overflow-hidden')
+      }
+    }
+  }
 }
 </script>
+
+<template>
+  <Teleport to="body">
+    <Transition name="modal">
+      <div
+        v-if="show"
+        :class="[
+          'fixed top-0 left-0 w-full h-full z-20 display-block overflow-x-hidden overflow-y-auto',
+          modalContentClass
+        ]"
+      >
+        <div class="w-screen max-w-none h-full m-0 relative">
+          <div class="relative flex flex-col w-full h-full">
+            <div class="overflow-y-auto relative grow shrink basis-auto">
+              <div
+                class="p-11 my-3 hidden md:flex shrink-0 items-center justify-between"
+              >
+                <!-- <div></div>
+                <div>
+                  <button
+                    @click="$emit('close')"
+                    class="p-2 border-2 text-white/50 hover:text-white hover:bg-neutral-light border-white/50 transition duration-300"
+                    ref="target"
+                  >
+                    Close
+                  </button>
+                </div> -->
+              </div>
+              <slot />
+              <div class="w-full text-center py-10">
+                <button
+                  @click="$emit('close')"
+                  :class="[
+                    'p-2 border-2 transition duration-300',
+                    buttonClass ||
+                      'bg-secondary hover:bg-neutral border-neutral text-neutral hover:text-secondary'
+                  ]"
+                >
+                  Close
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Transition>
+  </Teleport>
+</template>
 
 <style scoped>
 .modal-enter-active,
 .modal-leave-active {
-    @apply transition-all duration-300 ease-out;
+  @apply transition-all duration-300 ease-out;
 }
 
 .modal-enter-from,
 .modal-leave-to {
-    @apply opacity-0;
+  @apply opacity-0;
 }
 </style>
