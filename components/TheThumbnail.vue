190697c46edabc146f3a1d26447be232908e5c52
Peter V <peter@vavro.me>
2023-02-14 17:08:47 -0500

    Set fix size on images

--- components/TheThumbnail.vue
+++ components/TheThumbnail.vue
@@ -26,6 +26,7 @@ const props = defineProps({
         :src="`img/${props.thumbnailImage}`"
         :alt="props.title"
         width="100"
+        height="100"
         :class="[props.thumbnailImageClasses || '']"
       />
     </div>
