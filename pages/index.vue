05b46b5501930d737b9cf9f01f61b09bf1307c99
Peter V <peter@vavro.me>
2023-05-11 16:58:52 -0500

    Fix linter issue

--- pages/index.vue
+++ pages/index.vue
@@ -1,34 +1,36 @@
 <template>
-  <Head>
-    <Title> Peter Vavro = Full-Stack Engineer/Scrum Master </Title>
-  </Head>
-  <SectionIntro class="section" />
-  <SectionAboutMe id="about-me" class="section py-20" />
-  <TheSection
-    id="technologies"
-    title="My experience"
-    class="section py-20 text-primary"
-    titleClass="lg:text-4xl lg:-rotate-90 lg:-translate-x-[28rem] lg:-translate-y-24"
-  >
-    <ListTechnologies />
-  </TheSection>
-  <TheSection
-    id="code-samples"
-    title="Coding samples"
-    class="section py-20 text-tertiary"
-    titleClass="lg:text-4xl lg:-rotate-90 lg:-translate-x-[28rem] lg:-translate-y-16"
-  >
-    <ListCodeSamples />
-  </TheSection>
-  <TheSection
-    id="projects"
-    title="Things I've helped build"
-    class="section py-20 text-secondary"
-    titleClass="lg:text-4xl lg:-rotate-90 lg:-translate-x-[28rem] lg:translate-y-[10rem]"
-  >
-    <ListProjects />
-  </TheSection>
-  <BackgroundAnimation />
+  <div>
+    <Head>
+      <Title>Peter Vavro = Full-Stack Engineer</Title>
+    </Head>
+    <SectionIntro class="section" />
+    <SectionAboutMe id="about-me" class="section py-20" />
+    <TheSection
+      id="technologies"
+      title="My experience"
+      class="section py-20 text-primary"
+      titleClass="lg:text-4xl lg:-rotate-90 lg:-translate-x-[28rem] lg:-translate-y-24"
+    >
+      <ListTechnologies />
+    </TheSection>
+    <TheSection
+      id="code-samples"
+      title="Coding samples"
+      class="section py-20 text-tertiary"
+      titleClass="lg:text-4xl lg:-rotate-90 lg:-translate-x-[28rem] lg:-translate-y-16"
+    >
+      <ListCodeSamples />
+    </TheSection>
+    <TheSection
+      id="projects"
+      title="Things I've helped build"
+      class="section py-20 text-secondary"
+      titleClass="lg:text-4xl lg:-rotate-90 lg:-translate-x-[28rem] lg:translate-y-[10rem]"
+    >
+      <ListProjects />
+    </TheSection>
+    <BackgroundAnimation />
+  </div>
 </template>
 <style>
 :target {
