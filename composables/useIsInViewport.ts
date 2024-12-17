a1d1ff4ca6cd849569d685f4a1db3c98a32400ae
Peter Vavro <peter@vavro.me>
2024-12-17 13:39:57 -0500

    feat: install Nuxt

--- composables/useIsInViewport.ts
+++
@@ -1,8 +0,0 @@
-import { useElementVisibility } from '@vueuse/core'
-
-export const useIsInViewport = () => {
-  const target = ref(null)
-  const targetIsVisible = useElementVisibility(target)
-
-  return { target, isVisible: targetIsVisible }
-}
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,49 +1,5 @@
-// import { defineNuxtConfig } from 'nuxt'
-import svgLoader from 'vite-svg-loader'
-
-// https://v3.nuxtjs.org/api/configuration/nuxt.config
+// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  app: {
-    head: {
-      charset: 'utf-8',
-      viewport: 'width=device-width, initial-scale=1',
-      htmlAttrs: {
-        lang: 'en'
-      },
-      meta: [
-        {
-          name: 'description',
-          content:
-            'Peter Vavro is a Full-Stack Engineer, in to programming since 1994, specialized in web&mobile technologies.'
-        },
-        {
-          name: 'keywords',
-          content:
-            'Peter,Vavro,full-stack,developer,engineer,computer programmer,programmer,back-end,front-end,,backend,frontend,programming,personal,website,presentation, technologies,history,scrum master'
-        },
-        { name: 'author', content: 'Peter Vavro' },
-        { name: 'copyright', content: 'Peter Vavro' }
-      ],
-      link: [
-        { rel: 'icon', type: 'image/x-icon', href: 'favicon.ico' },
-        { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
-        {
-          rel: 'preconnect',
-          href: 'https://fonts.gstatic.com',
-          crossorigin: ''
-        },
-        {
-          rel: 'stylesheet',
-          href: 'https://fonts.googleapis.com/css2?family=Roboto+Mono&family=Staatliches&display=swap',
-          crossorigin: ''
-        }
-      ]
-    }
-  },
-  modules: [
-    '@nuxtjs/tailwindcss',
-    '@vueuse/nuxt',
-    '@nuxt/image-edge',
-    'nuxt-svgo'
-  ]
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true }
 })
