d7bff397f8c424f2650ce9a839e34e8d91f48f1b
Peter Vavro <peter@vavro.me>
2024-12-24 11:41:13 -0500

    refactor: move console log from default layout to app.vue for better organization

--- app.vue
+++ app.vue
@@ -1,5 +1,15 @@
 <template>
   <NuxtLayout>
-    <NuxtPage/>
+    <NuxtPage />
   </NuxtLayout>
 </template>
+
+<script>
+export default {
+  mounted() {
+    console.log(
+      "Hey 👋! You're probably wondering what could be the code behind my web page, so feel free to check it out at https://github.com/petervavro/vavro.me . Also, you can contact me at peter@vavro.me"
+    )
+  }
+}
+</script>
